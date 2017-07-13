pearljt/GRDB.swift

commit fb29227f4fff07abcc8c22321e39459c7a92735a

Raise a fatal error when DatabasePool is used in a reentrant way

GRDB/Utils/Pool.swift
@@ -36,44 +36,59 @@ import Dispatch
 ///     got 1
 ///     got 3
 final class Pool<T> {
-    var makeElement: (() throws -> T)?
-    private var items: [PoolItem<T>] = []
-    private let queue: DispatchQueue         // protects items
+    private struct Item {
+        let element: T
+        var available: Bool
+        
+        init(element: T, available: Bool) {
+            self.element = element
+            self.available = available
+        }
+        
+        mutating func getElementAndMakeUnavailable() -> T {
+            available = false
+            return element
+        }
+    }
+    
+    private let makeElement: () throws -> T
+    private var items: ReadWriteBox<[Item]> = ReadWriteBox([])
     private let semaphore: DispatchSemaphore // limits the number of elements
     
-    init(maximumCount: Int, makeElement: (() throws -> T)? = nil) {
+    init(maximumCount: Int, makeElement: @escaping () throws -> T) {
         GRDBPrecondition(maximumCount > 0, "Pool size must be at least 1")
         self.makeElement = makeElement
-        self.queue = DispatchQueue(label: "GRDB.Pool")
         self.semaphore = DispatchSemaphore(value: maximumCount)
     }
     
-    /// Returns a tuple (element, releaseElement())
-    /// Client MUST call releaseElement() after the element has been used.
+    /// Returns a tuple (element, release)
+    /// Client MUST call release() after the element has been used.
     func get() throws -> (T, () -> ()) {
-        var item: PoolItem<T>! = nil
+        var element: T! = nil
+        var index: Int! = nil
         _ = semaphore.wait(timeout: .distantFuture)
         do {
-            try queue.sync {
-                if let availableItem = items.first(where: { $0.available }) {
-                    item = availableItem
-                    item.available = false
+            try items.write { items in
+                if let availableIndex = items.index(where: { $0.available }) {
+                    index = availableIndex
+                    element = items[index].getElementAndMakeUnavailable()
                 } else {
-                    item = try PoolItem(element: makeElement!(), available: false)
-                    items.append(item)
+                    element = try makeElement()
+                    items.append(Item(element: element, available: false))
+                    index = items.count - 1
                 }
             }
         } catch {
             semaphore.signal()
             throw error
         }
-        let unlock = {
-            self.queue.sync {
-                item.available = true
+        let release = {
+            self.items.write { items in
+                items[index].available = true
             }
             self.semaphore.signal()
         }
-        return (item.element, unlock)
+        return (element, release)
     }
     
     /// Performs a synchronous block with an element. The element turns
@@ -85,9 +100,9 @@ final class Pool<T> {
     }
     
     /// Performs a block on each pool element, available or not.
-    /// The block is run is some arbitrary queue.
+    /// The block is run is some arbitrary dispatch queue.
     func forEach(_ body: (T) throws -> ()) rethrows {
-        try queue.sync {
+        try items.read { items in
             for item in items {
                 try body(item.element)
             }
@@ -102,19 +117,9 @@ final class Pool<T> {
     /// Empty the pool. Currently used items won't be reused.
     /// Eventual block is executed before any other element is dequeued.
     func clear(andThen block: () throws -> ()) rethrows {
-        try queue.sync {
-            items = []
+        try items.write { items in
+            items.removeAll()
             try block()
         }
-    }
-}
-
-private class PoolItem<T> {
-    let element: T
-    var available: Bool
-    
-    init(element: T, available: Bool) {
-        self.element = element
-        self.available = available
     }
 }

GRDB/Core/DatabasePool.swift
@@ -62,10 +62,8 @@ public final class DatabasePool {
         // Readers
         readerConfig = configuration
         readerConfig.readonly = true
-        readerConfig.defaultTransactionKind = .deferred // Make it the default. Other transaction kinds are forbidden by SQLite in read-only connections.
-        
-        readerPool = Pool<SerializedDatabase>(maximumCount: configuration.maximumReaderCount)
-        readerPool.makeElement = { [unowned self] in
+        readerConfig.defaultTransactionKind = .deferred // Make it the default for readers. Other transaction kinds are forbidden by SQLite in read-only connections.
+        readerPool = Pool(maximumCount: configuration.maximumReaderCount, makeElement: { [unowned self] in
             let reader = try SerializedDatabase(
                 path: path,
                 configuration: self.readerConfig,
@@ -81,7 +79,7 @@ public final class DatabasePool {
             }
             
             return reader
-        }
+        })
     }
     
     #if os(iOS)
@@ -191,7 +189,7 @@ public final class DatabasePool {
     
     fileprivate let writer: SerializedDatabase
     fileprivate var readerConfig: Configuration
-    fileprivate let readerPool: Pool<SerializedDatabase>
+    fileprivate var readerPool: Pool<SerializedDatabase>! = nil // var and nil-initialized so that we can use `self` when creating readerPool in DatabasePool.init()
     
     fileprivate var functions = Set<DatabaseFunction>()
     fileprivate var collations = Set<DatabaseCollation>()
@@ -250,11 +248,12 @@ extension DatabasePool : DatabaseReader {
     /// - throws: The error thrown by the block, or any DatabaseError that would
     ///   happen while establishing the read access to the database.
     public func read<T>(_ block: (Database) throws -> T) throws -> T {
-        // The block isolation comes from the DEFERRED transaction.
-        // See DatabasePoolTests.testReadMethodIsolationOfBlock().
+        preconditionNotReentrantRead()
         return try readerPool.get { reader in
             try reader.sync { db in
                 var result: T? = nil
+                // The block isolation comes from the DEFERRED transaction.
+                // See DatabasePoolTests.testReadMethodIsolationOfBlock().
                 try db.inTransaction(.deferred) {
                     result = try block(db)
                     return .commit
@@ -291,8 +290,55 @@ extension DatabasePool : DatabaseReader {
     /// - throws: The error thrown by the block, or any DatabaseError that would
     ///   happen while establishing the read access to the database.
     public func unsafeRead<T>(_ block: (Database) throws -> T) throws -> T {
+        preconditionNotReentrantRead()
         return try readerPool.get { reader in
             try reader.sync(block)
+        }
+    }
+    
+    /// Forbid the following code:
+    ///
+    ///     try dbPool.read { _ in
+    ///         try dbPool.read { _ in
+    ///         }
+    ///     }
+    ///
+    /// Why? Because this leads to a dead-lock, when the maximum number of
+    /// readers has been reached. And also because DatabaseQueue forbids
+    /// reentrancy as well, through its own internal SerializedDatabase.
+    ///
+    /// This method is not perfect, because we can't notice the following kind
+    /// of reentrancy:
+    ///
+    ///     try dbPool.read { _ in
+    ///         someQueue.sync {
+    ///             try dbPool.read { _ in
+    ///             }
+    ///         }
+    ///     }
+    private func preconditionNotReentrantRead() {
+        // Look for a reader for which reader.onValidQueue is true, which means
+        // that the current dispatch queue can be used for accessing its
+        // database: this is exactly what we want to prevent.
+        
+        var readers: [SerializedDatabase] = []
+        readerPool.forEach { reader in
+            // We can't check for reader.onValidQueue here because
+            // Pool.forEach() runs its closure argument in some arbitrary
+            // dispatch queue. We thus extract the reader so that we can query
+            // it below.
+            readers.append(reader)
+        }
+        
+        // Now the readers array contains some readers. The pool readers may
+        // already be different, because some other thread may have started 
+        // a new read, for example.
+        //
+        // This doesn't matter: the reader we are looking for is already on
+        // its own dispatch queue. If it exists, is still in use, thus still
+        // in the pool, and thus still relevant for our check:
+        for reader in readers where reader.onValidQueue {
+            fatalError("Database methods are not reentrant.")
         }
     }
     

GRDB/Core/SerializedDatabase.swift
@@ -149,14 +149,10 @@ final class SerializedDatabase {
         }
     }
     
-    /// Returns an optional database connection. If not nil, the caller is
-    /// executing on the serialized dispatch queue.
-    var availableDatabaseConnection: Database? {
-        guard let watchdog = SchedulingWatchdog.current else { return nil }
-        guard watchdog.allows(db) else { return nil }
-        return db
+    /// Returns true if any only if the current dispatch queue is valid.
+    var onValidQueue: Bool {
+        return SchedulingWatchdog.allows(db)
     }
-
     
     /// Executes the block in the current queue.
     ///

GRDB/Utils/ReadWriteBox.swift
@@ -12,17 +12,17 @@ final class ReadWriteBox<T> {
         self.queue = DispatchQueue(label: "GRDB.ReadWriteBox", attributes: [.concurrent])
     }
     
-    func read<U>(_ block: (T) -> U) -> U {
+    func read<U>(_ block: (T) throws -> U) rethrows -> U {
         var result: U? = nil
-        queue.sync {
-            result = block(self._value)
+        try queue.sync {
+            result = try block(self._value)
         }
         return result!
     }
     
-    func write(_ block: (inout T) -> Void) {
-        queue.sync(flags: [.barrier]) {
-            block(&self._value)
+    func write(_ block: (inout T) throws -> Void) rethrows {
+        try queue.sync(flags: [.barrier]) {
+            try block(&self._value)
         }
     }
     

commit 6ec2648149edffd70eef433336ba816a26c413a0

Fix Pool bug introduced in fb29227f4fff07abcc8c22321e39459c7a92735a
I think this is the bug behind this failed test suite: https://travis-ci.org/groue/GRDB.swift/jobs/253093331

In fb29227f4fff07abcc8c22321e39459c7a92735a Pool has started relying on *indexes* inside its inner array of items. Those indexes weren't reliable enough: this commit brings back independence from indexes by making the Item type a class again.

GRDB/Utils/Pool.swift
@@ -36,18 +36,13 @@ import Dispatch
 ///     got 1
 ///     got 3
 final class Pool<T> {
-    private struct Item {
+    private class Item {
         let element: T
         var available: Bool
         
         init(element: T, available: Bool) {
             self.element = element
             self.available = available
-        }
-        
-        mutating func getElementAndMakeUnavailable() -> T {
-            available = false
-            return element
         }
     }
     
@@ -64,18 +59,17 @@ final class Pool<T> {
     /// Returns a tuple (element, release)
     /// Client MUST call release() after the element has been used.
     func get() throws -> (T, () -> ()) {
-        var element: T! = nil
-        var index: Int! = nil
         _ = semaphore.wait(timeout: .distantFuture)
+        var item: Item! = nil
         do {
             try items.write { items in
-                if let availableIndex = items.index(where: { $0.available }) {
-                    index = availableIndex
-                    element = items[index].getElementAndMakeUnavailable()
+                if let availableItem = items.first(where: { $0.available }) {
+                    item = availableItem
+                    item.available = false
                 } else {
-                    element = try makeElement()
-                    items.append(Item(element: element, available: false))
-                    index = items.count - 1
+                    let element = try makeElement()
+                    item = Item(element: element, available: false)
+                    items.append(item)
                 }
             }
         } catch {
@@ -83,12 +77,14 @@ final class Pool<T> {
             throw error
         }
         let release = {
-            self.items.write { items in
-                items[index].available = true
+            self.items.write { _ in
+                // This is why Item is a class, not a struct: so that we can
+                // release it without having to find in it the items array.
+                item.available = true
             }
             self.semaphore.signal()
         }
-        return (element, release)
+        return (item.element, release)
     }
     
     /// Performs a synchronous block with an element. The element turns